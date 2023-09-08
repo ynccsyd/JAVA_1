964af56a49116b9ef4668c48a7c9e34fe65cd313
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-08 17:10:11 +0300

    Added Calculator

---
+++ SwitchCaseCalculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class SwitchCaseCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double number1, number2, result;
+        char operator;
+
+        System.out.print("The first number:");
+        number1 = scanner.nextDouble();
+
+        System.out.print("The second number: ");
+        number2 = scanner.nextDouble();
+
+        System.out.print("Choose the operator( +,-,*,/ ): ");
+        operator = scanner.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                result = number1 + number2;
+                System.out.println(("Result: " + result));
+                break;
+            case '-':
+                result = number1 - number2;
+                System.out.println(("Result: " + result));
+                break;
+            case '*':
+                result = number1 * number2;
+                System.out.println(("Result: " + result));
+                break;
+            case '/':
+                if (number2 != 0) {
+                    result = number1 / number2;
+                    System.out.println(("Result: " + result));
+                } else {
+                    System.out.println("Error: number2 must not be zero!");
+                }
+                break;
+            default:
+                System.out.println("Operation is invalid");
+        }
+    }
+}
