eb46023ed19ae3ed91e9c04d9b8209576882ade1
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-08 16:37:38 +0300

    Added Switch Codes

--- SwitchCaseYapisi.java
+++ SwitchCaseYapisi.java
@@ -1,3 +1,23 @@
-public class SwitchCaseYapisi {
+import java.util.Scanner;
 
+public class SwitchCaseYapisi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int value = input.nextInt();
+        switch (value) {
+            case 1:
+                // Value 1
+                break;
+            case 2:
+                // Value 2
+                break;
+            case 3:
+                // Value 3
+                break;
+            default:
+                // Value is not invalid
+                break;
+
+        }
+    }
 }
