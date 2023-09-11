67e9d6e0485a36296802bf1d292ab0b7359b29b2
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-11 16:02:56 +0300

    Added DO WHİLE

--- While.java
+++ While.java
@@ -16,3 +16,20 @@ public class While {
         }
     }
 }
+
+// public static void main(String[] args) {
+// Scanner input =new Scanner(System.in);
+// int pass;
+// boolean askPassword=true;
+
+// do{
+// System.out.println("enter password:");
+// pass=input.nextInt();
+// if(pass==123){
+// System.out.println("Correct");
+// askPassword=false;
+// }else{
+// System.out.println("Wrong");
+// }
+// }while(askPassword);
+// }
