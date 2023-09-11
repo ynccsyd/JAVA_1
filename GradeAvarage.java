43946d412be3b2ae99ec5b16eb9e06e5ac995850
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-11 09:20:15 +0300

    Added Grade Avarage

---
+++ GradeAvarage.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GradeAvarage {
+    public static void main(String[] args) {
+        int math, physics, chemistry, music;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Math grade: ");
+        math = input.nextInt();
+
+        System.out.print("Physics grade: ");
+        physics = input.nextInt();
+
+        System.out.print("Chemistry grade: ");
+        chemistry = input.nextInt();
+
+        System.out.print("Music grade: ");
+        music = input.nextInt();
+
+    }
+}
