179d22b513c992d58bfeafc5ff4306d6386ebe02
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-11 09:52:49 +0300

    Added Weather Advice

---
+++ WeatherAdvice.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class WeatherAdvice {
+    public static void main(String[] args) {
+        int temperature;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the air temperature? :");
+        temperature = input.nextInt();
+
+        if (temperature < 5) {
+            System.out.println("You can go skiing!.");
+        }else if(temperature>=5 && temperature>=25){
+            if(temperature<=15){
+                System.out.println("You can go o the cinema!.");
+            }
+            if(temperature>=){
+                System.out.println("You can go to the picnic.");
+            }
+        }else{
+            System.out.println("You can go to the swimming.");
+        }
+
+    }
+
+}
