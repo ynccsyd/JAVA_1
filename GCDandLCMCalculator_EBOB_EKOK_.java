e7655708cac3b1d38e78f460e54feb5862fd18e9
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-13 11:46:06 +0300

    Added For

--- GCDandLCMCalculator_EBOB_EKOK_.java
+++ GCDandLCMCalculator_EBOB_EKOK_.java
@@ -28,3 +28,36 @@ public class GCDandLCMCalculator_EBOB_EKOK_ {
         input.close();
     }
 }
+
+// import java.util.Scanner;
+
+// public class GCDandLCMCalculator_EBOB_EKOK_ {
+// public static void main(String[] args) {
+// Scanner input = new Scanner(System.in);
+
+// System.out.println("Enter the first number: ");
+// int num1 = input.nextInt();
+
+// System.out.println("Enter the second number:");
+// int num2 = input.nextInt();
+
+// int gcd = 1;
+
+// for(int i=1;i<=(num1*num2); i++){
+// if(i%num1==0 && i%num2==0){
+// System.out.println("Least Common Divisor (EKOK):" + " " + i);
+// break;
+// }
+// }
+// System.out.println();
+// for (int k = num1; k >= 1; k--) {
+// if (num1 % k == 0 && num2 % k == 0) {
+// gcd = k;
+// System.out.println("Greatest Common Divisor (EBOB): " + " " + gcd);
+// break;
+// }
+// }
+
+// input.close();
+// }
+// }
