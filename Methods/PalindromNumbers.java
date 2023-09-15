35137ac33bf342fd94c6832050654864b809a101
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-15 14:55:29 +0300

    Added PAlindrom Number

---
+++ Methods/PalindromNumbers.java
@@ -0,0 +1,25 @@
+package Methods;
+
+public class PalindromNumbers {
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+        if (number == reverseNumber) {
+
+            System.out.println(number + " =" + reverseNumber);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrom(202));
+    }
+}
