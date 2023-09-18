1d0ae8ceb3ad33af7dac679548d27eadc822b65e
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-18 11:26:13 +0300

    Added

--- Methods/RecursiveExponentiation.java
+++ Methods/RecursiveExponentiation.java
@@ -26,3 +26,19 @@ public class RecursiveExponentiation {
         return base * calculatePower(base, expo - 1);
     }
 }
+
+// static int power(int base, int exp) {
+// int result = 1;
+
+// for (int i = 1; i <= exp; i++) {
+// result *= base;
+// }
+// return result;
+// }
+
+// public static void main(String[] args) {
+// // f(x)=a^b
+
+// System.out.println(power(2, 5));
+
+// }
