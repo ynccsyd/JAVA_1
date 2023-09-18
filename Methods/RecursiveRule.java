9a2fb9bc446cf7bb06e79442eb976a743aee70fe
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-18 15:21:27 +0300

    Added Recursive Sample

--- Methods/RecursiveRule.java
+++ Methods/RecursiveRule.java
@@ -3,23 +3,26 @@ package Methods;
 import java.util.Scanner;
 
 public class RecursiveRule {
+    public static void recursiveRule(int n, int limit) {
+        System.out.print(n + " ");
+        if (n <= 0) {
+            return;
+        }
+        if (n == limit) {
+            recursiveRule(n - 5, limit);
+        } else {
+            recursiveRule(n - 5, limit);
+        }
+        System.out.print(n + " ");
+    }
+
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         System.out.println("Enter a number: ");
-        int number = inp.nextInt();
+        int n1 = inp.nextInt();
+        System.out.println("N Say: " + n1);
         inp.close();
+        recursiveRule(n1, n1);
 
-        performRule(number);
-    }
-
-    public static void performRule(int number) {
-        if (number <= 0) {
-            System.out.println("The latest number: " + number);
-            return;
-        }
-        System.out.println("Sub :" + number);
-        performRule(number - 5);
-
-        System.out.println("");
     }
 }
