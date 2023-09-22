9b8d5488dfd9f7103bc6b61f940318ef6a9bd185
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-22 15:37:04 +0300

    Added Harmonic MEan

--- HarmonicMeanCalculator.java
+++ HarmonicMeanCalculator.java
@@ -1,3 +1,24 @@
-public class HarmonicMeanCalculator {
+import java.util.Scanner;
 
+public class HarmonicMeanCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int n = scanner.nextInt();
+
+        double harmonicSeries = calculateHarmonicSeries(n);
+
+        System.out.println("Harmonic Mean: " + harmonicSeries);
+
+        scanner.close();
+    }
+
+    public static double calculateHarmonicSeries(int n) {
+        double sum = 0.0;
+        for (int i = 1; i <= n; i++) {
+            sum += 1.0 / i;
+        }
+        return sum;
+    }
 }
