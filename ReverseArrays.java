b0efdfbfaee8694179a0bee9ca0594b1371625f0
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-22 11:28:34 +0300

    Added array

--- ReverseArrays.java
+++ ReverseArrays.java
@@ -1,5 +1,10 @@
+import java.util.List;
+
 public class ReverseArrays {
+
     static int[] reverse(int[] list){
+        int [] reverse=new int[list.length];
+        
         for (int i=0; j=list.length-1; i<list.length; i++; j--){
             reverse[i]=list[j];
         }
