095835324776d2dc070008160ad714058ced2b69
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-25 14:06:17 +0300

    Added Element Frequency

---
+++ ArrayElementFrequency.java
@@ -0,0 +1,28 @@
+public class ArrayElementFrequency {
+    public static void main(String[] args) {
+        int[] list = { 3, 7, 3, 3, 8, 8, 12, 99, 9, 1, 3, 12 };
+
+        for (int i = 0; i < list.length; i++) {
+            int element = list[i];
+            int frequency = 0;
+
+            for (int j = 0; j < list.length; j++) {
+                if (element == list[j]) {
+                    frequency++;
+                }
+            }
+
+            boolean calculated = false;
+            for (int k = 0; k < i; k++) {
+                if (element == list[k]) {
+                    calculated = true;
+                    break;
+                }
+            }
+
+            if (!calculated) {
+                System.out.println("Element " + element + " appears " + frequency + " times.");
+            }
+        }
+    }
+}
