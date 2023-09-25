a1004b480ce5f695d1a4ac2f62522f325fd198bc
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-25 12:39:40 +0300

    Added HackerRank Project

---
+++ HackerRank/Solution2/Solution2.java
@@ -0,0 +1,21 @@
+package HackerRank.Solution2;
+
+import java.util.Scanner;
+
+public class Solution2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
