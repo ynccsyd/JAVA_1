85862b097bdbbc1f6463fc7e913d856ae840d9de
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-26 15:48:52 +0300

    Added Palindrom

---
+++ Palindrom.java
@@ -0,0 +1,27 @@
+public class Palindrom {
+    static boolean isPalindrome(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j++;
+        }
+        return true;
+    }
+
+    static boolean isPalindrome2(String str) {
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+        return str.equals(reverse);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome2("abba"));
+    }
+}
