54335d14f164b0dab3111a96bdca11fa4f4380a8
ynccsyd <muhibbi_10_@hotmail.com>
2023-09-26 15:03:37 +0300

    Added isWin

--- PredictionNumber.java
+++ PredictionNumber.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class PredictionNumber {
     public static void main(String[] args) {
@@ -6,7 +8,40 @@ public class PredictionNumber {
         int number = rand.nextInt(100);
 
         Scanner input = new Scanner(System.in);
+        int right = 0;
+        int selected;
+        int[] wrong = new int[5];
+        boolean isWin = false;
+        // System.out.println(number);
+
+        while (right < 5) {
+            System.out.println("Enter your prediction : ");
+            selected = input.nextInt();
+            if (selected < 0 || selected > 90) {
+                System.out.println("Please enter a number between 0-100.");
+                continue;
+            }
+            if (selected == number) {
+                System.out.println("Congrats...! Your prediction is: " + number);
+                isWin = true;
+                break;
+            } else {
+                System.out.println("Try again...!");
+                if (selected > number) {
+                    System.out.println(selected + "  is bigger than number");
+                } else {
+                    System.out.println(selected + "  is smaller than number");
+                }
+                wrong[right++] = selected;
+                System.out.println("Rest of your right : " + (5 - right));
+            }
+
+        }
         System.out.println(number);
+        if (isWin) {
+            System.out.println("I am sory, you have lost.");
+            System.out.println("Your predictions : " + Arrays.toString(wrong));
+        }
 
     }
 }
